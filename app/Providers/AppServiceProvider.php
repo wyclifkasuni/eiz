239c187a257c9d22bf55ac7abe00c9b46c0944bc
wyclifkasuni <60786038+wyclifkasuni@users.noreply.github.com>
2022-07-21 10:31:14 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //share data to dashboard
-
+        Schema::defaultStringLength(191);
         View::composer('layouts.dashboard.adm',DataComposer::class);
    
         View::composer('layouts.dashboard.user',DataComposer::class);
