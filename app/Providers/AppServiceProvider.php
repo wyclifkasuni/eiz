44dc236d809f62296d4a829dd5dc85621e07cae6
unknown <wyclifkasuni@gmail.com>
2022-07-21 10:53:54 +0200

    okay

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Http\View\Composers\DatComposer;
 use App\Http\View\Composers\DataComposer;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         //share data to dashboard      
 
         View::composer('layouts.dashboard.adm',DataComposer::class);
